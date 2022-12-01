b6e3c566b11119b4692419ff8688a03d5966cad9
Elmir Khalilov <edik.halilov1999@gmail.com>
2022-12-01 22:14:57 +0400

    Save users choice, fix little unperfections

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]'); 
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+const favicon = document.getElementById('favicon');
+
+function changeImageMode(color){
+  image1.src = `img/undraw_proud_coder_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+function setDarkMode(){
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  favicon.setAttribute('href', 'img/favicon-dark.png')
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  changeImageMode('dark');
+}
+
+function setLightMode(){
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  favicon.setAttribute('href', 'img/favicon-light.png');
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  changeImageMode('light');
+}
 
 function switchTheme(event){
   if (event.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark'); //read about doc.docElement
+    document.documentElement.setAttribute('data-theme', 'dark'); 
+    localStorage.setItem('theme', 'dark');
+    setDarkMode();
   }else{
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    setLightMode();
   }
 }
 
 toggleSwitch.addEventListener('change', switchTheme); 
+
+const currentTheme = localStorage.getItem('theme'); 
+if (currentTheme){
+  document.documentElement.setAttribute('data-theme', currentTheme); 
+
+  if (currentTheme === 'dark'){
+    toggleSwitch.checked = true;
+    setDarkMode();
+  }
+}
